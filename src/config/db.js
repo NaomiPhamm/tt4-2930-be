358c313fb8a72f89ddc65fad49f2e77292badf02
NaomiPhamm <nhipham2622k5@gmail.com>
2026-04-08 22:04:10 -0400

    update mongo connection to env

--- src/config/db.js
+++ src/config/db.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    const mongoUri = "mongodb+srv://nhipham2622k5_db_user:123456abc@cluster0.gdvpxia.mongodb.net/?appName=Cluster0/tt4-2930";
-
+  const mongoUri = process.env.MONGO_URI;
     await mongoose.connect(mongoUri);
     console.log("MongoDB connected successfully.");
 };
